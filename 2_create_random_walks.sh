0e70ac7889f8800739e39f2f8d48a44a53c87a95
Nikolai Rozanov <nikolai.rozanov@gmail.com>
2020-03-13 20:03:26 +0000

    fixing the second script

--- 2_create_random_walks.sh
+++ 2_create_random_walks.sh
@@ -2,10 +2,10 @@
 
 RANDOM_WALKS_SCRIPTS=randomwalks_utility
 
+mkdir -p 'randomwalks'
+
 # Preprocess the relations
 python3.6 $RANDOM_WALKS_SCRIPTS/preprocess_cn.py
-
-mkdir -p 'randomwalks'
 
 # Create the randomwalks using node2vec
 python3.6 $RANDOM_WALKS_SCRIPTS/random_walks.py

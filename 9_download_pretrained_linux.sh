9a03629efa3bd5a14aa3989a9e5f7f94dae05c01
Nikolai Rozanov <nikolai.rozanov@gmail.com>
2020-03-23 01:46:40 +0000

    download script

--- 9_download_pretrained_linux.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-waws --downloadS3 -f omcs_pretraining_free_wo_nsp_adapter.zip -b wluper-retrograph
-unzip omcs_pretraining_free_wo_nsp_adapter.zip
-mv omcs_pretraining_free_wo_nsp_adapter.zip models
-mv omcs_pretraining_free_wo_nsp_adapter models
